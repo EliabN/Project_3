5477c64f8c6cdb9b2f67e1abde855bea79e84afe
eliabn <30064396@tafe.wa.edu.au>
2023-11-13 22:40:12 +1030

    Updated typeDef.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,27 @@
 // typeDefs.js file
+const typeDefs = `
+    type User {
+     _id: ID
+     username: String
+     email: String
+     password: String
+    }
+
+    type Query {
+     users: [User]
+     user(username: String!): User
+    }
+    
+    type Auth {
+     token: ID!
+     user: User
+    }
+
+    type Mutation {
+     addUser(username: String!, email: String!, password: String!): Auth
+     login(email: String!, password: String!): Auth
+  }
+`;
+
+//teams: [Team]!
+module.exports = typeDefs;
