9cf704f587337919e8829798c37941970ec60772
eliabn <30064396@tafe.wa.edu.au>
2023-11-13 22:42:33 +1030

    Added comments to auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,17 +1,30 @@
+// auth.js
 const { GraphQLError } = require('graphql');
+
+// Import 'jsonwebtoken' for (JWT)JSON Web Tokens 
 const jwt = require('jsonwebtoken');
 
+// Key used to sign and verify JWTs tokens
 const secret = 'mysecretssshhhhhhh';
+
+// Token expiration time set to 2 hours
 const expiration = '2h';
 
+// Export AuthenticationError and signToken function
 module.exports = {
+  // Error if authentication fails
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
     extensions: {
       code: 'UNAUTHENTICATED',
     },
   }),
+
+  // Function to sign a JWT with user info
   signToken: function ({ email, username, _id }) {
+    // Payload with user info
     const payload = { email, username, _id };
+
+    // Sign JWT with payload, secret key, and expiration
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
