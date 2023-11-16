3e5db4f74305500fc423ab00319459aa2a769b8f
eliabn <30064396@tafe.wa.edu.au>
2023-11-16 23:26:03 +1030

    Updated index.html and commented App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
+// Import the main styling for the app
 import './App.css';
+// Import Outlet to render nested routes
 import { Outlet } from 'react-router-dom';
+// Import components from Apollo Client fir GraphQL
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+// Apollo Client instance with GraphQL URI and cache
 const client = new ApolloClient({
-  uri: '/graphql',
+  // GraphQL server is served at '/graphql' endpoint
+  uri: '/graphql', 
   cache: new InMemoryCache(),
 });
 
+// Define the main App component
 function App() {
+  // Wrap App with ApolloProvider for GraphQL functions
   return (
     <ApolloProvider client={client}>
+      {/* Main container div with flex layout */}
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        {/* Render the nested routes defined in your routing setup */}
         <Outlet />
       </div>
     </ApolloProvider>
   );
 }
 
+// Export App
 export default App;
