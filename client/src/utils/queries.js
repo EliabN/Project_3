0a2e1172cb2008b3a3fab0445e0db5f61b953c4d
eliabn <30064396@tafe.wa.edu.au>
2023-11-18 13:53:38 +1030

    created auth.js in utils and Updated queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,29 @@
 // queries
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      teams {
+        _id
+        name
+        league
+        coach
+      }
+    }
+  }
+`;
+
+export const QUERY_TEAM = gql`
+  query getTeam {
+    teams {
+      _id
+      name
+      league
+      coach    
+    }
+  }
+`;
