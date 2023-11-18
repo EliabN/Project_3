05c323ecd7c649f915c41246db841cb12c39284f
eliabn <30064396@tafe.wa.edu.au>
2023-11-18 13:26:01 +1030

    Added comments to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,32 +1,42 @@
+// Import necessary modules from React and the routing library
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// Import components and pages components
 import App from './App.jsx';
 import Home from './pages/Home';
-import Matchup from './pages/Team.jsx';
-import Vote from './pages/Fixtures.jsx';
+import Team from './pages/Team.jsx';
+import Fixtures from './pages/Fixtures.jsx';
 import NotFound from './pages/NotFound';
 
+// Create a BrowserRouter and define routes for your app
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
+    // App component as the root element
+    element: <App />, 
+    // Displayed in case of routing errors
+    errorElement: <NotFound />, 
     children: [
       {
-        index: true,
-        element: <Home />
+        // Rendered for the root path '/'
+        index: true, 
+        // Home component rendered as root path
+        element: <Home /> 
       }, {
-        path: '/matchup',
-        element: <Matchup />
+        // Route for team details with ':id'
+        path: '/team/:id', 
+        // Team component rendered for team details
+        element: <Team /> 
       }, {
-        path: '/matchup/:id',
-        element: <Vote />
+        path: '/fixtures/:id', 
+        element: <Fixtures /> 
       },
     ],
   },
 ]);
 
+// Using ReactDOM to render the app, providing the root element with RouterProvider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
