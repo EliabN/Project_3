b1be646aacb8b772dbbfd79a9b0db653bbb97c8c
eliabn <30064396@tafe.wa.edu.au>
2023-11-25 00:11:42 +1030

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB using the provided URI or a local default URI
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/scoresphere');
 
 // Export Mongoose connection
 module.exports = mongoose.connection;
